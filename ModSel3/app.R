4edf9889cb79764bcaf34d5f667b709f5ad0eba8
m-fattah <41021044+m-fattah@users.noreply.github.com>
2018-08-15 11:17:56 +0200

    Added auxiliary statistics to the data frame.

--- ModSel3/app.R
+++ ModSel3/app.R
@@ -16,6 +16,8 @@ library(markdown)
 suppressMessages(library(dplyr))
 
 MONTHS_PRIOR <- 12
+ARRIVALS_PERCENT_ACCURACY_LOWER_BOUND <- .75
+ARRIVALS_PERCENT_ACCURACY_UPPER_BOUND <- 1.1
 
 #################################################
 # Google Sheet Preparation
@@ -67,7 +69,7 @@ findIndex <- function(dfInput, valueInput) {
 
 # Method: getDataFrame - Returns a subset of a given model's data, limited to the given dates
 getDataFrame <- function(modelDataColumn, startRowIndex) {
-  return(modelDataColumn[startRowIndex, startRowIndex + 11])
+  return(modelDataColumn[startRowIndex, startRowIndex + (MONTHS_PRIOR - 1)])
 }
 
 # Method: getModelSheetName - Returns the name of the model formatted like the Google Sheets column names
@@ -75,9 +77,54 @@ getModelSheetName <- function(modelName) {
   return(cat("modelarrivals_", modelName, "arrivals"))
 }
 
+# Method: addArrivalsDiffPercent ??? Adds a column for the arrivals difference and a column for the arrivals percentage accuracy
+addArrivalsDiffPercent <- function(dateActualArrModelArr) {
+  arrivalsDiff <- dateActualArrModelArr$modelArrivals - dateActualArrModelArr$actualArrivals
+  arrivalsPercentage <- dateActualArrModelArr$modelArrivals / dateActualArrModelArr$actualArrivals
+  return(data.frame(dateActualArrModelArr, arrivalsDiff, arrivalsPercentage))
+}
+
+# Method: 
+
 # Method: addOtherCorrelationsStats - 
+addOtherCorrelationsStats <- function(dateActualArrModelArr) {
+  dfDateActualModelDiffPercent <- addArrivalsDiffPercent(dateActualArrModelArr)
+  
+  # add a column to indicate whether the actual arrivals percentage accuracy lies between the lower and upper 
+  #  bounds (constants at the top). 1 if yes, 0 if no.
+  percentIsWithinRange <- NULL
+  for (i in 1:length(dfDateActualModelDiffPercent$arrivalsPercentage)) {
+    if ((ARRIVALS_PERCENT_ACCURACY_LOWER_BOUND < dfDateActualModelDiffPercent$arrivalsPercentage[i]) & (dfDateActualModelDiffPercent$arrivalsPercentage[i] < ARRIVALS_PERCENT_ACCURACY_UPPER_BOUND)) {
+      percentIsWithinRange[i] <- 1
+    }
+    else {
+      percentIsWithinRange[i] <- 0
+    }
+  }
+  dfDateActualModelDiffPercentScore <- data.frame(dfDateActualModelDiffPercent, percentIsWithinRange)
+### TODO: consider 2 points vs. 1 point in the scoring above  
+  
+  # add a column for last year % accuracy
+  yearPercentAcc <- mean(dfDateActualModelDiffPercentScore$arrivalsPercentage)
+  dfDateActualModelDiffPercentScoreYearly <- data.frame(dfDateActualModelDiffPercentScore, yearPercentAcc)
+  
+  # add a column for PCC values
+  PCC <- sum(percentIsWithinRange/length(percentIsWithinRange))
+  dfDateActualModelDiffPercentScoreYearlyPCC <- data.frame(dfDateActualModelDiffPercentScoreYearly, PCC)
+   
+  return(dfDateActualModelDiffPercentScoreYearlyPCC)
+}
 
 
+checkTest <- 
+  for (i in 1:length(longTest)) {
+  if ((.75 < longTest[i]) & (longTest[i] < 1.1)) {
+    blah[i] <- 1
+  }
+    else {
+      blah[i] <- 0
+    }
+}
 
 
 
@@ -98,9 +145,9 @@ getModelSheetName <- function(modelName) {
 #}
 
 #tableize <- function(selectedModels) {
-  
+
 #  tables <- list() # create a list to hold all tables
-  
+
 #  for (selectedModel in selectedModels) { # go through all possible values of variables
 #    table <- getDataFrame(selectedModel, arg2, ...)
 #    tables[[as.character(selectedModel)]] <- 
@@ -135,9 +182,9 @@ ui <- fluidPage(
       dataTableOutput("x1Table")
     )
     
-#    fluidRow(
-#
-#    )
+    #    fluidRow(
+    #
+    #    )
   )
 )
 
@@ -169,23 +216,26 @@ server <- function(input, output) {
     plot(head(cars, input$n), main="Foo")
   }, bg = "#F5F5F5")
   
-#  output$correlationsDataTables <- renderUI({
+  #  output$correlationsDataTables <- renderUI({
   
-#### must go into the tableize function because modelName is a variable from a vector
-#
-#  currentSheet <- gs_read(correlationSheet, ws = worksheet())
-#  startDate <- findStartDate(MONTHS_PRIOR)
-#  startDateRowIndex <- findIndex(mdy(currentSheet$Date), startDate)
-#  dates <- getDataFrame(currentSheet$Date, startDateRowIndex)
-#  actualArrivals <- getDataFrame(currentSheet$actualarrivals, startDateRowIndex)
-#  modelSheetName <- getModelSheetName(modelName)
-#  modelArrivals <- getDataFrame(currentSheet$modelSheetName, startDateRowIndex)
-#
-####
+  #### must go into the tableize function because modelName is a variable from a vector
+  #
+  currentSheet <- gs_read(correlationSheet, ws = worksheet())
+  startDate <- findStartDate(MONTHS_PRIOR)
+  startDateRowIndex <- findIndex(mdy(currentSheet$Date), startDate)
+  dates <- getDataFrame(currentSheet$Date, startDateRowIndex)
+  actualArrivals <- getDataFrame(currentSheet$actualarrivals, startDateRowIndex)
+  modelAlternateName <- getModelSheetName(modelName)
+  modelArrivals <- getDataFrame(currentSheet$modelAlternateName, startDateRowIndex)
+  dateActualArrModelArr <- data.frame(dates, actualArrivals, modelArrivals)
+  dataFrameWithStats <- addOtherCorrelationsStats(dateActualArrModelArr)
+  
+  #
+  ####
   
   
-#    tableize(rownames(input$x1Table_rows_selected))
-#  })
+  #    tableize(rownames(input$x1Table_rows_selected))
+  #  })
   
 }
 
